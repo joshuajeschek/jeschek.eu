768b94d9018496ce28e142c4a0b7be14ebd6ae81
Joshua Jeschek <dev@jeschek.eu>
2024-02-23 09:21:18 +0000

    refactor(photos): favicon script

--- photos/favicons.sh
+++ photos/favicons.sh
@@ -10,78 +10,36 @@ fi
 replacement_color_hex=$1
 file_list="favicons.txt" # The file containing the list of image paths in the Docker container
 resize_list="resize.txt" # The file containing the list of image paths for resizing
-favicons_folder="./.favicons" # Folder to store processed images and backups
+FAVICONS="./.favicons" # Folder to store processed images and backups
 
-# Create the favicons directory if it doesn't exist
-mkdir -p "$favicons_folder"
 
 # Read the file line by line
 while IFS= read -r line; do
     echo "Processing $line..."
-    file_name=$(basename "$line")
-    target_path="$favicons_folder/$file_name"
-    backup_path="$target_path.bak"
-
-    # Copy file from Docker container to the .favicons folder
-    docker compose cp "immich-server:$line" "$target_path"
-
-    # Create a backup copy with .bak extension
-    if [ ! -f "$backup_path" ]; then
-        echo "Creating backup for $line..."
-        cp "$target_path" "$backup_path"
-    else
-        echo "Backup already exists for $line, skipping backup creation."
-    fi
-
-    # Apply the Python script
-    ./replace_color.py "$target_path" "$replacement_color_hex" "$target_path"
-
-    # Copy the modified file back to the container
-    docker compose cp "$target_path" "immich-server:$line"
-
+    file_path="immich-server:$line"
+    target_path=$(../.scripts/save_copy.sh get "$file_path" "$FAVICONS")
+    ../.scripts/replace_color.py "$target_path" "$replacement_color_hex" "$target_path"
+    ../.scripts/save_copy.sh set "$file_path" "$FAVICONS"
     echo "Processed and copied back $line"
 done < "$file_list"
 
 # Resize favicon.png according to resize.txt instructions
 while IFS= read -r line; do
     echo "Resizing and processing $line..."
-    file_name=$(basename "$line")
-
-    # Extract size from the file path
+    file_path="immich-server:$line"
     size=$(echo "$line" | grep -o '[0-9]\+')
-    target_path="$favicons_folder/$file_name"
-    backup_path="$target_path.bak"
-
-    docker compose -p photos cp "immich-server:$line" "$target_path"
-
-    # Create a backup copy with .bak extension
-    if [ ! -f "$backup_path" ]; then
-        echo "Creating backup for $line..."
-        cp "$target_path" "$backup_path"
-    else
-        echo "Backup already exists for $line, skipping backup creation."
-    fi
-
-    # Resize favicon.png to the specified size and save to target path
-    convert "$favicons_folder/favicon.png" -resize "${size}x${size}" "$target_path"
-
-    docker compose -p photos cp "$target_path" "immich-server:$line"
-
+    target_path=$(../.scripts/save_copy.sh get "$file_path" "$FAVICONS")
+    convert "$FAVICONS/favicon.png" -resize "${size}x${size}" "$target_path"
+    ../.scripts/save_copy.sh set "$file_path" "$FAVICONS"
     echo "Resized and processed $line"
 done < "$resize_list"
 
 
-path='/usr/src/app/www/favicon.ico'
-file_name=$(basename $path)
-target_path="$favicons_folder/$file_name"
-backup_path="$target_path.bak"
-docker compose -p photos cp "immich-server:$path" "$target_path"
-
-if [ ! -f "$backup_path" ]; then
-    cp "$target_path" "$backup_path"
-fi
-
-convert "$favicons_folder/favicon.png" -resize 256x256 -define icon:auto-resize=64,48,32,16 "$target_path"
-docker compose -p photos cp "$target_path" "immich-server:$path"
+echo "Creating favicon.ico"
+file_path='immich-server:/usr/src/app/www/favicon.ico'
+target_path=$(../.scripts/save_copy.sh get "$file_path" "$FAVICONS")
+convert "$FAVICONS/favicon.png" -resize 256x256 -define icon:auto-resize=64,48,32,16 "$target_path"
+../.scripts/save_copy.sh set "$file_path" "$FAVICONS"
+echo "Created favicon.ico"
 
 echo "All files processed and resized as requested."
